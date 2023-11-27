a7217d434643f21c67df627036652cea4356796b
mslmyrtd <mslmyrtd@gmail.com>
2023-11-27 23:07:32 +0300

    book model created and first posst request sended

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,36 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+
 const app = express();
+app.use(express.json());
+
+app.post("/", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 mongoose
   .connect(mongoDBURL)
