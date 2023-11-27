88ba9adf90aed312e6c6e05013732f167790c371
mslmyrtd <mslmyrtd@gmail.com>
2023-11-27 22:54:21 +0300

    moongose added for connection created packet.json for start

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5000;
+export const mongoDBURL =
+  "mongodb+srv://root:2544955_@cluster0.miu0lub.mongodb.net/?retryWrites=true&w=majority";
---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+const app = express();
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
