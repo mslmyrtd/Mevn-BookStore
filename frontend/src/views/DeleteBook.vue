ffa7cb155984e6a4ab690738216b81b0ca57b1b4
mslmyrtd <mslmyrtd@gmail.com>
2023-11-29 23:47:30 +0300

    deleteBook page completed

--- frontend/src/views/DeleteBook.vue
+++ frontend/src/views/DeleteBook.vue
@@ -1,5 +1,50 @@
-<template>
-  <div>DElete</div>
-</template>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter, useRoute } from "vue-router";
+import BackButton from "@/components/BackButton.vue";
+import Spinner from "@/components/Spinner.vue";
+const router = useRouter();
+const route = useRoute();
+const loading = ref(false);
 
-<script setup></script>
+const handleDeleteBook = async () => {
+  loading.value = true;
+  try {
+    await axios.delete(`http://localhost:5000/books/${route.params.id}`);
+    loading.value = false;
+    router.push("/");
+  } catch (error) {
+    loading.value = false;
+    console.log(error);
+  }
+};
+const handleCancel = () => router.push("/");
+</script>
+<template>
+  <div class="p-4">
+    <BackButton />
+    <h1 class="text-3xl my-4">Delete Book</h1>
+    <div v-if="loading">
+      <Spinner />
+    </div>
+    <div
+      class="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto"
+    >
+      <h3 class="text-2xl">Are you sure you want to delete this book?</h3>
+
+      <button
+        class="p-4 bg-red-600 text-white m-8 w-full"
+        @click="handleDeleteBook"
+      >
+        Yes, delete it
+      </button>
+      <button
+        class="p-4 bg-green-600 text-white m-8 w-full"
+        @click="handleCancel"
+      >
+        Cancel
+      </button>
+    </div>
+  </div>
+</template>
