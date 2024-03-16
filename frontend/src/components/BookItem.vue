ddccb108f6586a09b5d775478e8adea139ff1bf0
mslmyrtd <mslmyrtd@gmail.com>
2024-03-17 00:14:06 +0300

    child comp created for books

---
+++ frontend/src/components/BookItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg relative mt-10">
+    <img
+      class="h-64 w-96"
+      src="https://images.unsplash.com/photo-1513001900722-370f803f498d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Ym9va3N8ZW58MHx8MHx8fDA%3D"
+      alt="Sunset in the mountains"
+    />
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+      <p class="text-gray-700 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+        quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+        nihil.
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+        >#photography</span
+      >
+      <span
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+        >#travel</span
+      >
+      <span
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+        >#winter</span
+      >
+    </div>
+    <div class="flex justify-between align-middle px-6 py-1">
+      <a href="#">
+        <span
+          class="inline-flex items-center gap-x-1.5 rounded-full bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600"
+        >
+          <svg
+            class="h-1.5 w-1.5 fill-gray-400"
+            viewBox="0 0 6 6"
+            aria-hidden="true"
+          >
+            <circle cx="3" cy="3" r="3" />
+          </svg>
+          19.12.2005
+        </span></a
+      >
+      <a
+        href="#"
+        class="inline-flex justify-end items-center px-2 mb-1 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Read more
+        <svg
+          class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+      </a>
+    </div>
+    <span
+      class="absolute top-0 translate-y-2 bg-slate-500 rounded-full p-2 right-0 text-lime-50 border-2 border-gray-200"
+      >7.6</span
+    >
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ frontend/src/components/BookList.vue
@@ -0,0 +1,11 @@
+<script setup>
+import BookItem from '@/components/BookItem.vue';
+</script>
+
+<template>
+  <div v-for="i in 4" :key="i" class="flex justify-center">
+    <BookItem />
+  </div>
+</template>
+
+<style scoped></style>
